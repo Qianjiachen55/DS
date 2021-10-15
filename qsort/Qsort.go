e76f61b6c6e06bfc7f232a8e7d2cac4618443b7e
Qianjiachen55 <qianjiachen@happywater.xyz>
2021-10-15 23:20:25 +0800

    feat(qsort): add qsort 10.15

---
+++ qsort/Qsort.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+)
+
+//var count int = 0
+
+func main() {
+	input := []int{6, 5, 2, 7, 3, 9, 8, 4, 10, 1}
+	fmt.Println("input:")
+	fmt.Println(input)
+	lenth := len(input) - 1
+
+	qsort(input, 0, lenth)
+
+	fmt.Println("after sort")
+	fmt.Println(input)
+}
+
+func qsort(arr []int, start int, end int) {
+	if start > end {
+		return
+	} else {
+		index := partition(arr, start, end)
+		qsort(arr, start, index-1)
+		qsort(arr, index+1, end)
+	}
+}
+
+func partition(arr []int, start int, end int) int {
+	left := start
+	right := end
+	temp := arr[start]
+	for left < right {
+		for left < right && arr[right] >= temp {
+			right -= 1
+			//从右边找比temp小的
+		}
+		for left < right && arr[left] <= temp {
+			left += 1
+			//从左边找比temp大的
+		}
+		arr[right], arr[left] = arr[left], arr[right]
+	}
+	arr[start], arr[left] = arr[left], arr[start]
+	//count++
+	//fmt.Println("count: ", count)
+	//fmt.Println("arr: ", arr)
+	//fmt.Println("right:", right, ", left:", left)
+	return left
+
+	return right
+}
