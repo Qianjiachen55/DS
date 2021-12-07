e5bc3679671af31bc36ee5c224282918eccdb3be
Qianjiachen55 <qianjiachen@happywater.xyz>
2021-12-07 22:55:06 +0800

    add(70): 金典斐波那契数列，非递归写法

---
+++ 70 爬楼梯/main.go
@@ -0,0 +1,22 @@
+package main
+
+func climbStairs(n int) int {
+	if n==1{
+		return 1
+	}
+	if n==2{
+		return 2
+	}
+	if n==3{
+		return 3
+	}
+	a := 1
+	b := 2
+	ans := 0
+	for i:=3;i<=n;i++{
+		ans = a+b
+		a = b
+		b = ans
+	}
+	return ans
+}
